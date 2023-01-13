3c86b7ffff3240f307dc421a4409c32fcb823780
Bruno Morais <brunomoraisads@gmail.com>
2023-01-13 10:56:26 -0300

    Pokedex

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <div class="card" id="find-card">
+        <h1 class="is-size-3">Pokedex</h1>
+        <input id="find-input" class="input is-rounded" type="text" placeholder="Buscar Pokemon" v-model="find">
+        <button class="button is-fullwidth is-success is-outlined" @click="findMethod">Buscar</button>
+      </div>
+      <div v-for="(poke, index) in filteredPokemon" :key="poke.url">
+      <Pokemon-item :name="poke.name" :url="poke.url" :num="index+1"/>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import PokemonItem from './components/PokemonItem.vue'
 
 export default {
+  data(){
+    return{
+      pokemons: [],
+      filteredPokemon: [],
+      find: ''
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    PokemonItem
+  },
+  created: async function(){
+    try{
+      const pokemonsRes = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      this.pokemons = pokemonsRes.data.results
+      this.filteredPokemon = pokemonsRes.data.results
+    }catch(err){
+      console.log(err)
+    }
+  }, 
+  methods: {
+    findMethod(){
+      if(this.find == "" || this.find == " "){
+        this.filteredPokemon = this.pokemons
+      }else{
+        this.filteredPokemon = this.pokemons.filter(pokemon => pokemon.name == this.find)
+        console.log(this.find, this.filteredPokemon)
+      }
+    }
+  },
+  computed: {
+    /*
+    findResult: function(){
+      if(this.find == "" || this.find == " "){
+        return this.pokemons
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.find)
+      }
+    }
+    */
   }
+  
 }
 </script>
 
@@ -22,5 +71,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#find-card{
+  padding: 10px;
+  margin-bottom: 5px;
+}
+#find-input{
+  margin-bottom: 6px;
 }
 </style>
---
+++ src/components/PokemonItem.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="content">
+        <div class="card">
+            <div class="card-image">
+                <figure>
+                    <img :src="currentImg" alt="Placeholder image" />
+                    <img />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">{{ num }} - {{ upperCase }}</p>
+                        <p class="subtitle is-6">{{ pokemon.type }}</p>
+                    </div>
+                </div>
+                <div class="content">
+                    <button @click="changeSprite" class="button is-danger is-outlined is-small is-fullwidth">Mudar Sprite</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            isFront: true,
+            currentImg: '',
+            pokemon: {
+                type: "",
+                frontImg: "",
+                backImg: "",
+            },
+        };
+    },
+    created: async function () {
+        try {
+            const res = await axios.get(this.url);
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.frontImg = res.data.sprites.front_default;
+            this.pokemon.backImg = res.data.sprites.back_default;
+            this.currentImg = this.pokemon.frontImg
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    props: {
+        name: String,
+        url: String,
+        num: Number,
+    },
+    computed: {
+        upperCase() {
+            const newName =
+            this.name[0].toUpperCase() + this.name.slice(1, this.name.length);
+            return newName;
+        },
+    },
+    methods: {
+        changeSprite(){
+            if(this.isFront){
+                this.isFront = false
+                this.currentImg = this.pokemon.backImg
+            }else{
+                this.isFront = true
+                this.currentImg = this.pokemon.frontImg
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+#content {
+    margin-bottom: 5px;
+}
+</style>
